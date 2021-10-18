85117b7e5baaf8edcb40236aaf77f5e925d29cd4
Saeid Gholami <saeidxgholami@gmail.com>
2021-10-18 17:41:28 +0330

    Find longest line to draw dashes based on that value

--- viewer.py
+++ viewer.py
@@ -1,6 +1,10 @@
 import csv
 
 
+# TODO:
+# Distribute each cell equally in the length of the row.
+
+
 def read_data(filename):
     with open(filename, "r") as csv_file:
         return list(csv.reader(csv_file))
@@ -9,16 +13,21 @@ def read_data(filename):
 def display(data):
     output = ""
     lines = []
-    biggest_length = 0
+    lengths = []
+    biggest_len = 0
     for row in data:
         # clean white space around row item then join them with pipe symbole.
         line = " | ".join(item.strip() for item in row)
-        line = f"| {line} | \n"
-        if len(line) > biggest_length:
-            biggest_length = len(line)
+        line = f"| {line} |"
+        lines.append(line)
+        lengths.append(len(line))
 
-        line += "-" * biggest_length
-        output += "\n" + line
+    biggest_len = max(lengths)
+    for line in lines:
+        out = "".join(line) + "\n" + "-" * biggest_len
+        print(out)
+    # line += "-" * biggest_length
+    # output += "\n" + line
 
     print(output)
 
